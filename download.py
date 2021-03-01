4e0b6194daf90d1db746694748205586e8c2af10
TheGhostOfInky <69942208+TheGhostOfInky@users.noreply.github.com>
2021-03-01 18:59:54 +0000

    Added intial files

---
+++ download.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 02:22:23 2020
+
+@author: TheGhostOfInky
+"""
+import sys
+fname = sys.argv[1]
+links = open (fname,"r")
+pshell = open ("downloader.ps1","a")
+pshell.write ("mkdir JSON")
+pshell.write ("\n")
+for line in links:
+    field = line.split(";")
+    url = field[0]
+    name = field[1]
+    pshell.write ("Invoke-WebRequest ")
+    pshell.write ('\"')
+    pshell.write (url)
+    pshell.write ('\"')
+    pshell.write ('-OutFile .\\JSON\\')
+    pshell.write (name)
